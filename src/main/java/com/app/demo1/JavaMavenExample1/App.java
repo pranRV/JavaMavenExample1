ec513ab14862ac685d4a2eb76cc0e505c1e18835
pranRV <pranava.rchs@gmail.com>
2024-11-14 15:32:05 +0530

    SAMPLE JAVA EXAMPLE1

---
+++ src/main/java/com/app/demo1/JavaMavenExample1/App.java
@@ -0,0 +1,10 @@
+package com.app.demo1.JavaMavenExample1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Pranava1");
+    }
+}
